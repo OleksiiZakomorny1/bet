ce52495cd5bf9ef48a1c0b893904b1bf3d891d3c
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-05 21:16:01 +0200

    finish js

---
+++ script.js
@@ -0,0 +1,43 @@
+const resultCell = document.querySelector('.result')
+const resultBtn = document.querySelector('button')
+const betCells = document.querySelectorAll('.bet')
+const pointCells = document.querySelectorAll('.points')
+
+resultBtn.onclick = handleResults
+
+function handleResults() {
+	// generate and show result
+	const result = `${generate()}:${generate()}`
+	resultCell.innerText = result
+
+	// watch for each bet
+	for (let i = 0; i < betCells.length; i++) {
+		const cell = betCells[i]
+		const bet = cell.innerText
+
+		// calculate points
+		const points = calcPoints(result, bet)
+		pointCells[i].innerText = points
+	}
+}
+
+function generate() {
+	return Math.floor(Math.random() * 10)
+}
+
+function calcPoints(result, bet) {
+	if (result === bet) {
+		return 3
+	}
+
+	result = result.split(/ *: */)
+	bet = bet.split(/ *: */)
+	const resultSign = Math.sign(result[0] - result[1])
+	const betSign = Math.sign(bet[0] - bet[1])
+
+	if (resultSign === betSign) {
+		return 1
+	}
+
+	return 0
+}
